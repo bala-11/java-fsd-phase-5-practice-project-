8a191a3e9e4e2bfa2b051b3b2bee9023f7de5d17
Balaji <balajikalimuthu482@gmail.com>
2022-10-12 17:35:17 +0530

    First Commit

---
+++ springdummy/src/main/java/com/example/demo/SpringdummyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdummyApplication.class, args);
+	}
+
+}
